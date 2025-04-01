f23b2fa4c3ddf1c1fd304f5574e676f460dfb0c0
Colin Drouineau <colin.drouineau@etu.minesparis.psl.eu>
2025-04-01 23:07:58 +0200

    move files mastered

--- skills_to_acquire/move_files.py
+++ skills_to_acquire/move_files.py
@@ -1,11 +1,14 @@
 import os
 import shutil
 
+
 def move_and_rename_file(source_path, destination_path):
     try:
         # Move and rename the file
         shutil.move(source_path, destination_path)
-        print(f"File moved and renamed successfully from {source_path} to {destination_path}")
+        print(
+            f"File moved and renamed successfully from {source_path} to {destination_path}"
+        )
     except FileNotFoundError:
         print(f"Error: The file {source_path} does not exist.")
     except PermissionError:
@@ -13,27 +16,15 @@ def move_and_rename_file(source_path, destination_path):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-# Example usage
-source_path = '/path/to/your/sourcefile.txt'  # Replace with your source file path
-destination_path = '/path/to/your/destinationfile.txt'  # Replace with your destination file path
-move_and_rename_file(source_path, destination_path)
-
-import os
 
 def create_folder(folder_path):
-    try:
+    try: # ALREADY EXISTED  os.path.isdir(directory_path): if os.path.isfile(file_path):
         # Create the directory
         os.makedirs(folder_path, exist_ok=True)
         print(f"Folder created successfully at {folder_path}")
     except Exception as e:
         print(f"An error occurred while creating the folder: {e}")
 
-# Example usage
-folder_path = '/path/to/your/newfolder'  # Replace with your desired folder path
-create_folder(folder_path)
-
-
-import os
 
 def delete_empty_folder(folder_path):
     try:
@@ -43,6 +34,22 @@ def delete_empty_folder(folder_path):
     except OSError as e:
         print(f"Error: {folder_path} : {e.strerror}")
 
+
 # Example usage
-folder_path = '/path/to/your/emptyfolder'  # Replace with the path of the empty folder you want to delete
+source_path = "C:\\colin_films\\time_mastersiki.uwu"
+destination_path = "C:\\colin_films\\TIME_MASTERS\\time_masters.mkv"  # Replace with your source file path
+move_and_rename_file(source_path, destination_path)
+
+
+'''
+
+file_path = "C:\\colin_films\\TIME_MASTERS\\time_masters.mkv"
+# Example usage
+folder_path = "C:\\colin_films\\TIME_MASTERS_newhaha"  # Replace with your desired folder path
+create_folder(folder_path)
+
+
+# Example usage
+folder_path = "C:\\colin_films\\TIME_MASTERS_newhaha"  # Replace with the path of the empty folder you want to delete
 delete_empty_folder(folder_path)
+'''
