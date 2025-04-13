26ab2760056ba2d2e33097b7e9531df77472d554
Colin Drouineau <colin.drouineau@etu.minesparis.psl.eu>
2025-04-13 11:41:52 +0200

    Starting to implement main record function

--- record_launcher.py
+++ record_launcher.py
@@ -2,12 +2,19 @@ import os
 
 
 def record(path_to_disk, new_name=None):
+    # if new disque dur : do you want to rename it ?
+    
+    # How to detect it's new : mettre dans "Other" un fichier txt "testé", avec :
+    # Le nombre de films dans le disque, le code pour revenir à la config initiale (ainsi que les titres), 
+    # Date du record
 
-    # Specify the directory path
-    directory_path = "/path/to/your/directory"
+    # fonction qui parcourt et arrange tout, et en même temps que le parcours, remplit la base de données
+    # la fonction doit aussi écrire le fichier "testé"
+
+
 
     # List all files and directories in the specified path
-    entries = os.listdir(directory_path)
+    entries = os.listdir(path_to_disk)
 
     for entry in entries:
         print(entry)
--- utils.py
+++ utils.py
@@ -14,3 +14,6 @@ import chardet
 #
 #     content = content.split("\n")
 #     return content
+
+def get_extension(file_name):
+    return file_name.split(".")[-1]
